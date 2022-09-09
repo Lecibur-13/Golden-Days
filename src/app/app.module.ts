9a667cb30fd1107c16acdccef9d89a8f10757d75
Rubicel Perez <rubicelperezcordova@gmail.com>
2022-09-08 23:39:16 -0500

    first build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { LandingPageModule } from "./landing-page/landing-page.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NgbCarouselModule, NgbModule, NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,12 @@ import {NgbCarouselModule, NgbModule, NgbPaginationModule} from '@ng-bootstrap/n
     NgbCarouselModule,
     NgbPaginationModule,
     NgbModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
